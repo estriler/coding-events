4028de0e9af96abbf18fcba9e8843a51848ab078
Eric S <estriler@hotmail.com>
2024-10-07 13:22:27 -0500

    created and rendered a form

--- src/main/java/org/launchcode/codingevents/controllers/HomeController.java
+++ src/main/java/org/launchcode/codingevents/controllers/HomeController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("index")
+    @GetMapping
     public String index(){
-
+        return "index";
     }
 
 }
